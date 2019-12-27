220b755ea5cc8accefd9342af0e6f5d28015db9a
mohamedbrahimi <mohkriss3@gmail.com>
2019-12-28 00:09:00 +0100

    - review build function - move from padding to center

--- lib/clock_face.dart
+++ lib/clock_face.dart
@@ -11,49 +11,19 @@ class ClockFace extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return new Padding(
-      padding: const EdgeInsets.all(10.0),
-      child: new AspectRatio(
-        aspectRatio: 1.0,
-        child: new Container(
-          width: double.infinity,
-          decoration: new BoxDecoration(
-            shape: BoxShape.circle,
-            color: Colors.white,
-          ),
-
-          child: new Stack(
-            children: <Widget>[
-              //dial and numbers
-              new Container(
-                width: double.infinity,
-                height: double.infinity,
-                padding: const EdgeInsets.all(10.0),
-                child:new CustomPaint(
-                  painter: new ClockDialPainter(clockText: clockText),
-                ),
-              ),
-
-
-              //centerpoint
-              new Center(
-                child: new Container(
-                  width: 15.0,
-                  height: 15.0,
-                  decoration: new BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: Colors.black,
-                  ),
-                ),
-              ),
-
-
-              new ClockHands(dateTime:dateTime, showHourHandleHeartShape: showHourHandleHeartShape),
-
-            ],
+    return Center(
+      child: new Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: new AspectRatio(
+          aspectRatio: 0.75,
+          child: new Container(
+            width: double.infinity,
+            decoration: new BoxDecoration(
+              shape: BoxShape.circle,
+              color: Color(0xfff4f9fd),
+            ),
           ),
         ),
-
       ),
     );
   }
