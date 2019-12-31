2fcdc35c5557cd7555acf296772feac566bb9ac5
mohamedbrahimi <mohkriss3@gmail.com>
2019-12-31 23:05:33 +0100

    added new component and classes

--- lib/screens/second_screen.dart
+++ lib/screens/second_screen.dart
@@ -66,59 +66,191 @@ class SecondTab extends StatelessWidget {
                     ),
                   ),
                 ],
+              ),
+              Container(
+                height: 200,
+                width: 200,
+                decoration: BoxDecoration(
+                    color: Color(0xfff0f5fb),
+                    border: Border.all(
+                      width: 8,
+                      color: Color(0xfff0f5fb),
+                    ),
+                    borderRadius: BorderRadius.circular(3)),
+                padding: EdgeInsets.all(15),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      "THIS WEEK",
+                      style: TextStyle(
+                          fontSize: _smallFontSize,
+                          fontWeight: _smallFontWeight,
+                          letterSpacing: _smallFontSpacing,
+                          color: _fontColor),
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    Container(
+                      height: 120,
+                      padding: EdgeInsets.symmetric(horizontal: 10),
+                      width: double.infinity,
+                      child: CustomPaint(
+                        foregroundPainter: GraphPainter(),
+                      ),
+                    )
+                  ],
+                ),
               )
             ],
           ),
           SizedBox(
             height: 25,
           ),
-          Container(
-            padding: EdgeInsets.symmetric(vertical: 10),
-            decoration: BoxDecoration(
-                border: Border(
-              bottom: BorderSide(color: Color(0xffdde0f7), width: 1.5),
-            )),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text(
-                  "THURSDAY",
+          RecordItem(
+              fontColor: _fontColor,
+              smallFontSpacing: _smallFontSpacing,
+              day: "THURSDAY"),
+          RecordItem(
+              fontColor: _fontColor,
+              smallFontSpacing: _smallFontSpacing,
+              day: "FRIDAY"),
+          RecordItem(
+              fontColor: _fontColor,
+              smallFontSpacing: _smallFontSpacing,
+              day: "SATURDAY"),
+          RecordItem(
+              fontColor: _fontColor,
+              smallFontSpacing: _smallFontSpacing,
+              day: "SUNDAY"),
+          RecordItem(
+              fontColor: _fontColor,
+              smallFontSpacing: _smallFontSpacing,
+              day: "MONDAY"),
+          RecordItem(
+              fontColor: _fontColor,
+              smallFontSpacing: _smallFontSpacing,
+              day: "WEDNESDAY"),
+        ],
+      ),
+    );
+  }
+}
+
+class GraphPainter extends CustomPainter {
+  Paint trackBarPaint = Paint()
+    ..color = Color(0xff818aab)
+    ..style = PaintingStyle.stroke
+    ..strokeCap = StrokeCap.round
+    ..strokeWidth = 12;
+
+  Paint trackPaint = Paint()
+    ..color = Color(0xffdee6f1)
+    ..style = PaintingStyle.stroke
+    ..strokeCap = StrokeCap.round
+    ..strokeWidth = 12;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    Path trackPath = Path();
+    Path trackBarPath = Path();
+
+    List val = [
+      size.height * 0.8,
+      size.height * 0.5,
+      size.height * 0.9,
+      size.height * 0.8,
+      size.height * 0.5,
+    ];
+
+    double origin = 8;
+    for (int i = 0; i < val.length; i++) {
+      trackPath.moveTo(origin, size.height);
+      trackPath.lineTo(origin, 0);
+
+      trackBarPath.moveTo(origin, size.height);
+      trackBarPath.lineTo(origin, val[i]);
+
+      origin = origin + size.width * 0.22;
+    }
+//    trackPath.moveTo(8, size.height);
+//    trackPath.lineTo(8, 0);
+//
+//    trackBarPath.moveTo(8, size.height);
+//    trackBarPath.lineTo(8, size.height * 0.5);
+
+    canvas.drawPath(trackPath, trackPaint);
+    canvas.drawPath(trackBarPath, trackBarPaint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    // TODO: implement shouldRepaint
+    return false;
+  }
+}
+
+class RecordItem extends StatelessWidget {
+  const RecordItem({
+    Key key,
+    @required Color fontColor,
+    @required double smallFontSpacing,
+    @required String day,
+  })  : _fontColor = fontColor,
+        _smallFontSpacing = smallFontSpacing,
+        _day = day,
+        super(key: key);
+
+  final Color _fontColor;
+  final double _smallFontSpacing;
+  final String _day;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 10),
+      decoration: BoxDecoration(
+          border: Border(
+        bottom: BorderSide(color: Color(0xffdde0f7), width: 1.5),
+      )),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            _day,
+            style: TextStyle(
+                fontSize: 16, fontWeight: FontWeight.w700, color: _fontColor),
+          ),
+          SizedBox(
+            height: 15,
+          ),
+          Row(
+            children: <Widget>[
+              Text(
+                "01/10/2019",
+                style: TextStyle(
+                    fontSize: 13,
+                    fontWeight: FontWeight.w400,
+                    letterSpacing: _smallFontSpacing,
+                    color: _fontColor),
+              ),
+              SizedBox(
+                width: 25,
+              ),
+              Expanded(
+                child: Text(
+                  "45.3 MINUTES",
                   style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.w700,
-                      color: _fontColor),
+                    fontSize: 13,
+                    fontWeight: FontWeight.w400,
+                    letterSpacing: _smallFontSpacing,
+                    color: _fontColor,
+                  ),
                 ),
-                SizedBox(
-                  height: 15,
-                ),
-                Row(
-                  children: <Widget>[
-                    Text(
-                      "01/10/2019",
-                      style: TextStyle(
-                          fontSize: 13,
-                          fontWeight: FontWeight.w400,
-                          letterSpacing: _smallFontSpacing,
-                          color: _fontColor),
-                    ),
-                    SizedBox(
-                      width: 25,
-                    ),
-                    Expanded(
-                      child: Text(
-                        "45.3 MINUTES",
-                        style: TextStyle(
-                          fontSize: 13,
-                          fontWeight: FontWeight.w400,
-                          letterSpacing: _smallFontSpacing,
-                          color: _fontColor,
-                        ),
-                      ),
-                    )
-                  ],
-                )
-              ],
-            ),
+              )
+            ],
           )
         ],
       ),
