c066d7d5d605c793b5b0e80dfc3786c84cef7557
mohamedbrahimi <mohkriss3@gmail.com>
2020-01-05 21:57:32 +0100

    defined classes and functions

--- lib/screens/third_screen.dart
+++ lib/screens/third_screen.dart
@@ -248,5 +248,140 @@ class SheetContainer extends StatelessWidget {
       ),
     );
   }
+
+  Widget specifications(double sheetItemHeight) {
+    return Container(
+      padding: EdgeInsets.only(top: 15, left: 40),
+      child: Column(
+        children: <Widget>[
+          Text(
+            "Specifications",
+            style: TextStyle(
+                color: Colors.black, fontWeight: FontWeight.w700, fontSize: 10),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 15),
+            height: sheetItemHeight,
+            child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: currentCar.specifications.length,
+                itemBuilder: (context, index) {
+                  return ListItem(
+                    sheetItemHeight: sheetItemHeight,
+                    mapVal: currentCar.specifications[index],
+                  );
+                }),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget features(double sheetItemHeight) {
+    return Container(
+      padding: EdgeInsets.only(top: 15, left: 40),
+      child: Column(
+        children: <Widget>[
+          Text(
+            "Features",
+            style: TextStyle(
+                color: Colors.black, fontWeight: FontWeight.w700, fontSize: 10),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 15),
+            height: sheetItemHeight,
+            child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: currentCar.features.length,
+                itemBuilder: (context, index) {
+                  return ListItem(
+                    sheetItemHeight: sheetItemHeight,
+                    mapVal: currentCar.features[index],
+                  );
+                }),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget offerDetails(double sheetItemHeight) {
+    return Container(
+      padding: EdgeInsets.only(top: 15, left: 40),
+      child: Column(
+        children: <Widget>[
+          Text(
+            "Offer Details",
+            style: TextStyle(
+                color: Colors.black, fontWeight: FontWeight.w700, fontSize: 10),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 15),
+            height: sheetItemHeight,
+            child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: currentCar.offerDetails.length,
+                itemBuilder: (context, index) {
+                  return ListItem(
+                    sheetItemHeight: sheetItemHeight,
+                    mapVal: currentCar.offerDetails[index],
+                  );
+                }),
+          )
+        ],
+      ),
+    );
+  }
 }
 
+class ListItem extends StatelessWidget {
+  final double sheetItemHeight;
+  final Map mapVal;
+
+  ListItem({this.sheetItemHeight, this.mapVal});
+
+  @override
+  Widget build(BuildContext context) {
+    var innerMap;
+    bool isMap;
+
+    if (mapVal.values.elementAt(0) is Map) {
+      innerMap = mapVal.values.elementAt(0);
+      isMap = true;
+    } else {
+      innerMap = mapVal;
+      isMap = false;
+    }
+    return Container(
+      margin: EdgeInsets.only(right: 20),
+      width: sheetItemHeight,
+      height: sheetItemHeight,
+      padding: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(15),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          mapVal.keys.elementAt(0),
+          isMap
+              ? Text(
+                  innerMap.keys.elementAt(0),
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      color: Colors.black, letterSpacing: 1.2, fontSize: 11),
+                )
+              : Container(),
+          Text(
+            innerMap.values.elementAt(0),
+            textAlign: TextAlign.center,
+            style: TextStyle(
+                color: Colors.black, fontWeight: FontWeight.w600, fontSize: 15),
+          ),
+        ],
+      ),
+    );
+  }
+}
