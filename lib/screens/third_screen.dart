75638661ad3de005aa8c42a141094642d48ae9d6
mohamedbrahimi <mohkriss3@gmail.com>
2020-01-07 22:30:02 +0100

    defined 'RentButton'

--- lib/screens/third_screen.dart
+++ lib/screens/third_screen.dart
@@ -51,6 +51,34 @@ class LayoutStarts extends StatelessWidget {
   }
 }
 
+class RentButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.bottomRight,
+      child: SizedBox(
+        width: 200,
+        child: FlatButton(
+          onPressed: () {},
+          child: Text(
+            "Rent Car",
+            style: TextStyle(
+                color: Colors.white,
+                fontSize: 18,
+                letterSpacing: 1.4,
+                fontFamily: "arial"),
+          ),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(topLeft: Radius.circular(35)),
+          ),
+          color: Colors.black,
+          padding: EdgeInsets.all(25),
+        ),
+      ),
+    );
+  }
+}
+
 class CarDetailsAnimation extends StatefulWidget {
   @override
   _CarDetailsAnimationState createState() => _CarDetailsAnimationState();
