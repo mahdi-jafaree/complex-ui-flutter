e3461ea7dd3ac01925c9ef29e577bc1917e2aba4
mohamedbrahimi <mohkriss3@gmail.com>
2020-01-07 22:30:21 +0100

    defined '_CarDetailsAnimationState'

--- lib/screens/third_screen.dart
+++ lib/screens/third_screen.dart
@@ -84,7 +84,43 @@ class CarDetailsAnimation extends StatefulWidget {
   _CarDetailsAnimationState createState() => _CarDetailsAnimationState();
 }
 
-class _CarDetailsAnimationState extends State<CarDetailsAnimation> {
+class _CarDetailsAnimationState extends State<CarDetailsAnimation>
+    with TickerProviderStateMixin {
+  AnimationController fadeController;
+  AnimationController scaleController;
+
+  Animation fadeAnimation;
+  Animation scaleAnimation;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    fadeController =
+        AnimationController(duration: Duration(milliseconds: 180), vsync: this);
+
+    scaleController =
+        AnimationController(duration: Duration(milliseconds: 350), vsync: this);
+
+    fadeAnimation = Tween(begin: 0.0, end: 1.0).animate(fadeController);
+    scaleAnimation = Tween(begin: 0.8, end: 1.0).animate(CurvedAnimation(
+      parent: scaleController,
+      curve: Curves.easeInOut,
+      reverseCurve: Curves.easeInOut,
+    ));
+  }
+
+  forward() {
+    scaleController.forward();
+    fadeController.forward();
+  }
+
+  reverse() {
+    scaleController.reverse();
+    fadeController.reverse();
+  }
+
   @override
   Widget build(BuildContext context) {
     return CarDetails();
